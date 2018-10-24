e2d7ecf6b7e9ce9cad26edee9630c55dabc66724
cha1ra <s11397as@cha1ra.com>
2018-10-24 11:14:37 +0900

    検索画面を移動させて見た

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ var $doc = $(document);
 $doc.on('keydown', function(e){
     $('.test').html(e.which);
 })
+
+
+
+const len = $("#memo-list li").length;
+$("#save").on("click", function(){
+    for(let i=1; i<=len; i++){
+        const v = $("#memo"+i).val();
+        localStorage.setItem("memo"+i, v);
+    }
+});
+
+//2.ロード時に表示(for文)
+for(let i=1; i<=len; i++){
+    const v = localStorage.getItem("memo"+i);
+    $("#memo"+i.val(v));
+}
+
